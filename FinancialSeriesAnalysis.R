709f96e6f49d277ee1ded25037df0d4f3aee68c6
Udi Zabot <udi.zabott@gmail.com>
2023-12-06 19:46:55 -0400

    atualizado

--- FinancialSeriesAnalysis.R
+++ FinancialSeriesAnalysis.R
@@ -10,12 +10,12 @@
 pacman::p_load(tidyverse, quantmod, 
                PerformanceAnalytics, PortfolioAnalytics)
 
-
+# atualizado
 
 ###########################################################################
 # Getting Time Series ------------------------------------------------------
 
-tickers <- c("ITSA4.SA", "TAEE11.SA", "ABEV3.SA", "BBAS3.SA", "WEGE3.SA")
+tickers <- c("RURA11.SA", "RZAG11.SA", "XPCA11.SA", "KNCA11.SA", "JGPX11.SA", "EGAF11.SA", "DCRA11.SA", "BBGO11.SA", "VGIA11.SA")
 
 Prices <- getSymbols(tickers,
                      auto.assign = TRUE,
@@ -26,7 +26,7 @@ Prices <- getSymbols(tickers,
 
 
 ## Se preferir, converta os preços para períodicidade semanal
-WeekPrices <- to.weekly(Prices, indexAt = "lastof", OHLC = FALSE)
+WeekPrices <- to.daily(Prices, indexAt = "lastof", OHLC = FALSE)
 
 
 ###########################################################################
@@ -85,10 +85,10 @@ chart.Correlation(Returns)
 chart.CumReturns(Returns, wealth.index = T, legend.loc = "topleft")
 
 ## Histograma 
-chart.Histogram(Returns, breaks = 100)
+chart.Histogram(Returns$DCRA11.SA, breaks = 100)
 
 ## QQ Plot
-chart.QQPlot(Returns)
+chart.QQPlot(Returns$DCRA11.SA)
 
 ## Gráfico de Risco-Retorno
 chart.RiskReturnScatter(Returns)
